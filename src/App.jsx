44f319a504bc6a0e2b0d1ded8052416fa4b6ae48
GianMarco-20 <75840140gm20@gmail.com>
2025-08-30 14:08:34 -0500

    🚀 Primer commit: proyecto base con React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900">
+      <h1 className="text-5xl font-bold text-teal-400">
+        🚀 Tailwind está funcionando
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
