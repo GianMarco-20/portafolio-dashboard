c916f083cf13dbcf93b0ea7f38925f9456d27bf0
GianMarco-20 <75840140gm20@gmail.com>
2025-08-31 16:23:04 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// Configuración de Vite
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
