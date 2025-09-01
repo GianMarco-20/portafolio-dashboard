50109425bbdd98d7b6286268a1e01f92af17e271
GianMarco-20 <75840140gm20@gmail.com>
2025-09-01 10:38:44 -0500

    Create Habilidades.jsx

---
+++ frontend/src/pages/Habilidades.jsx
@@ -0,0 +1,6 @@
+// frontend/src/pages/Formacion.jsx
+import React from "react";
+
+export default function Formacion() {
+  return <h2 className="text-2xl font-bold">📚 Página Formación Académica</h2>;
+}
