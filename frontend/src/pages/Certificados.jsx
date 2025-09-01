c0730ae5518d0d2a1d88123a276cf78982edf5a7
GianMarco-20 <75840140gm20@gmail.com>
2025-09-01 10:38:33 -0500

    Create Certificados.jsx

---
+++ frontend/src/pages/Certificados.jsx
@@ -0,0 +1,6 @@
+// frontend/src/pages/Certificados.jsx
+import React from "react";
+
+export default function Certificados() {
+  return <h2 className="text-2xl font-bold">📜 Página Certificados</h2>;
+}
